aa80f39d8548195e4244530db3b41cc6cf213fee
sadagatasgarov <sedaqet.esgerov@gmail.com>
2020-07-10 01:17:03 +0400

    neredeyse hazir

--- Java.js
+++ Java.js
@@ -19,78 +19,77 @@ function computerPlay(){
 }
 
 //computerPlay();
-let rounds=0;
-let playerRound=0;
-let computerRound=0;
-function playRound(playerSelection,computerSelection){
+let rounds = 0;
+let playerRound = 0;
+let computerRound = 0;
+function playRound(playerSelection, computerSelection){
 
-    if (playerSelection=='Scissors') {
-        if(computerSelection=='Scissors') {
-          alert('Noone won');
+    if (playerSelection == 'Scissors' ) {
+        if(computerSelection == 'Scissors' ) {
+          alert( 'Noone won' );
           
             }
-            else if (computerSelection=='Paper') {
-                alert('You win!');
-                playerRound+=1;
+            else if (computerSelection == 'Paper' ) {
+                alert( 'You win!' );
+                playerRound += 1;
             }
             else {
-                 alert('You lose!');
-                 computerRound+=1;
+                 alert( 'You lose!' );
+                 computerRound += 1;
         }
     }
-    else if(playerSelection=='Paper'){
-        if(computerSelection=='Paper') {
-            alert('Noone won');
+    else if( playerSelection == 'Paper' ){
+        if( computerSelection == 'Paper' ) {
+            alert( 'Noone won' );
             }
-              else if (computerSelection=='Rock') {
-                  alert('You win!');
-                  playerRound+=1;
+              else if ( computerSelection=='Rock' ) {
+                  alert( 'You win!' );
+                  playerRound += 1;
                 }
               else { 
-                  alert('You lose!');
-                  computerRound+=1;
+                  alert( 'You lose!' );
+                  computerRound += 1;
                 
                 }  
     }
-    else if (playerSelection=='Rock') {
-        if (computerSelection=='Rock') {
-            alert('Noone won');
+    else if (playerSelection == 'Rock' ) {
+        if ( computerSelection == 'Rock' ) {
+            alert( 'Noone won' );
         }
-          else if (computerSelection=='Scissors') {
-              alert('You Win!');
-              playerRound+=1;
+          else if ( computerSelection == 'Scissors' ) {
+              alert( 'You Win!' );
+              playerRound += 1;
         }
           else {
-              alert('You Lost');
-              computerRound+=1;
+              alert( 'You Lost' );
+              computerRound += 1;
             }
     }
-    else{
-        alert('Please enter: Rock, Paper, Scissors');
-    }
+    
+    
 
     return 
 }
 
 function game(){
-    for (rounds=0; rounds <= 5; rounds++) {
+    for (rounds = 0; rounds <= 5; rounds++) {
     //const x = array[rounds]; 
-    const computerSelection=computerPlay();
+    const computerSelection = computerPlay();
     const playerSelection = prompt('Please enter: Rock, Paper, Scissors');
-    playRound(playerSelection,computerSelection);
+    playRound( playerSelection, computerSelection );
     alert(`Oyuncu-${playerSelection}, Komputer-${computerSelection}`);
     alert(`Kullanici ${playerRound}, Komputer ${computerRound}`);
 
 }
 
-if(playerRound>computerRound){
+if( playerRound > computerRound ){
     alert(`Kullanici kazandi: Kullanici ${playerRound}, Computer ${computerRound}`);
 }
-else if (playerRound<computerRound) {
+else if (playerRound < computerRound) {
     alert(`Bilgisayar kazandi: Kullanici ${playerRound}, Computer ${computerRound}`);
 }
 else{
-    alert('berabere');
+    game();
 }
 }
 game();
