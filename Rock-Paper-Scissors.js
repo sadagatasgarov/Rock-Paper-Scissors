22f7b6b54d328cbebc38a847ac7fe3e4fb0c8a71
sadagatasgarov <sedaqet.esgerov@gmail.com>
2020-07-11 00:48:46 +0400

    better than before

--- Rock-Paper-Scissors.js
+++ Rock-Paper-Scissors.js
@@ -26,27 +26,39 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection == 'scissors') {
         if (computerSelection == 'scissors') {
             alert('Noone won');
+            document.write(`Player-${playerSelection}, Computer-${computerSelection}<br>`);
+            document.write(`Player ${playerRound}, Computer ${computerRound}<br><br>`);
 
         }
         else if (computerSelection == 'paper') {
             alert('You win!');
+            document.write(`Player-${playerSelection}, Computer-${computerSelection}<br>`);
+            document.write(`Player ${playerRound}, Computer ${computerRound}<br><br>`);
             playerRound += 1;
         }
         else {
             alert('You lose!');
+            document.write(`Player-${playerSelection}, Computer-${computerSelection}<br>`);
+            document.write(`Player ${playerRound}, Computer ${computerRound}<br><br>`);
             computerRound += 1;
         }
     }
     else if (playerSelection == 'paper') {
         if (computerSelection == 'paper') {
             alert('Noone won');
+            document.write(`Player-${playerSelection}, Computer-${computerSelection}<br>`);
+            document.write(`Player ${playerRound}, Computer ${computerRound}<br><br>`);
         }
         else if (computerSelection == 'rock') {
             alert('You win!');
+            document.write(`Player-${playerSelection}, Computer-${computerSelection}<br>`);
+            document.write(`Player ${playerRound}, Computer ${computerRound}<br><br>`);
             playerRound += 1;
         }
         else {
             alert('You lose!');
+            document.write(`Player-${playerSelection}, Computer-${computerSelection}<br>`);
+            document.write(`Player ${playerRound}, Computer ${computerRound}<br><br>`);
             computerRound += 1;
 
         }
@@ -54,14 +66,20 @@ function playRound(playerSelection, computerSelection) {
     else if (playerSelection == 'rock') {
         if (computerSelection == 'rock') {
             alert('Noone won');
+            document.write(`Player-${playerSelection}, Computer-${computerSelection}<br>`);
+            document.write(`Player ${playerRound}, Computer ${computerRound}<br><br>`);
         }
         else if (computerSelection == 'scissors') {
             alert('You Win!');
             playerRound += 1;
+            document.write(`Player-${playerSelection}, Computer-${computerSelection}<br>`);
+            document.write(`Player ${playerRound}, Computer ${computerRound}<br><br>`);
         }
         else {
             alert('You Lost');
             computerRound += 1;
+            document.write(`Player-${playerSelection}, Computer-${computerSelection}<br>`);
+            document.write(`Player ${playerRound}, Computer ${computerRound}<br><br>`);
         }
     }
     return
@@ -73,8 +91,6 @@ function game() {
         const computerSelection = computerPlay();
         const playerSelection = prompt('Please enter: Rock, Paper, Scissors').toLowerCase();
         playRound(playerSelection, computerSelection);
-        document.write(`Player-${playerSelection}, Computer-${computerSelection}<br>`);
-        document.write(`Player ${playerRound}, Computer ${computerRound}<br><br>`);
     }
 
     if (playerRound > computerRound) {
